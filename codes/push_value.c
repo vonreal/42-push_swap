06e9c449dc06009877d465a1a8b32c8c5c2fbe82
Jiun Na <jna@c1r7s7.42seoul.kr>
2021-05-27 04:10:29 +0900

    [CREATE] 'push_value' fucntion initialize the stack and insert a valid value.

---
+++ codes/push_value.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_value.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jna <jna@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/27 02:11:10 by jna               #+#    #+#             */
+/*   Updated: 2021/05/27 04:08:14 by jna              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	*allocate(int size, int length)
+{
+	void		*temp;
+
+	if (!(temp = ft_calloc(size, length)))
+		exit(0);
+	return (temp);
+}
+
+t_stack		init_stack(int size)
+{
+	t_stack stack;
+
+	stack.list = (int *)allocate(sizeof(int), size);
+	stack.size = size;
+	stack.top = -1;
+	return (stack);
+}
+
+t_stack		push_value(int size, char **src)
+{
+	t_stack	stack;
+	int		i;
+
+	stack = init_stack(size);
+	i = 0;
+	while (src[i])
+	{
+		stack.list[i] = ft_atoi(src[i]);
+		i++;
+	}
+	stack.top = i - 1;
+	return (stack);
+}
