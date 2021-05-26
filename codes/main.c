1c847a572cd5ed0fee1459a511d8be8e951f2a24
Jiun Na <jna@c1r7s7.42seoul.kr>
2021-05-27 04:12:53 +0900

    [MODIFIED] norm check and added main.c

--- codes/main.c
+++ codes/main.c
@@ -3,29 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <jna@student.42.fr>                   +#+  +:+       +#+        */
+/*   By: jna <jna@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 13:25:56 by root              #+#    #+#             */
-/*   Updated: 2021/05/26 13:53:48 by root             ###   ########.fr       */
+/*   Updated: 2021/05/27 04:07:42 by jna              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../headers/push_swap.h"
+#include "push_swap.h"
 
-void	error_msg()
+int		main(int argc, char **argv)
 {
-}
+	t_stack		a;
+	t_stack		b;
 
-void	valid_arg(int argc, char **argv)
-{
-	
-}
-
-int	main(int argc, char **argv)
-{
-	t_stack	stack;
-	
 	valid_arg(argc, argv);
-	push_value(argc, argv);
-	return (1);
+	a = push_value(argc - 1, argv + 1);
+	b = init_stack(argc - 1);
+	/* TODO
+	** push_swap(): Study stack data struction and Make instructions.
+	*/
+	return (0);
 }
